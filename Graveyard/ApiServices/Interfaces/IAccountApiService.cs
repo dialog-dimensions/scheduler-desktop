bce5efa88b4c08ba75e02f7b2ec3d43e99e88124
dialog-dimensions <dialog.dimensions@gmail.com>
2024-03-24 14:22:20 +0200

    removed graveyard

--- Graveyard/ApiServices/Interfaces/IAccountApiService.cs
+++
@@ -1,7 +0,0 @@
-﻿// namespace Wpf.Scheduler.Services.ApiServices.Interfaces;
-//
-// public interface IAccountApiService
-// {
-//     Task RegisterAsync(string id, string userName, string phoneNumber, string password, string confirmPassword);
-//     Task LoginAsync(string id, string password);
-// }
